9441633688e0921c97da1de16a98d87979af02d2
majidpawar9 <majidpawar9@gmail.com>
2023-07-14 14:32:01 -0400

    Added ArrayAdapter and used ActivityMainBinding to bind all views

--- app/src/main/java/com/majid/todolist/MainActivity.kt
+++ app/src/main/java/com/majid/todolist/MainActivity.kt
@@ -7,11 +7,13 @@ import android.widget.TextView
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.Toast
 import com.majid.todolist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding :ActivityMainBinding
+    val userInputList = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +21,17 @@ class MainActivity : AppCompatActivity() {
         // This makes the views defined in activity_main.xml accessible
         setContentView(binding.root)
 
+        val arrayAdapter =
+            ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, userInputList)
+
+        val userString = binding.userInput.getText()
+
+        binding.addButton.setOnClickListener {
+            Toast.makeText(this,"Added " + userString,Toast.LENGTH_SHORT).show()
+            userInputList.add(binding.userInput.getText().toString())
+            binding.toDoList.setAdapter(arrayAdapter)
+
+        }
 
     }
 }
