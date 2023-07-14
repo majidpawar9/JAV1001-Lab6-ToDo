523705a2623d19317ebf2938e680652988972a10
majidpawar9 <majidpawar9@gmail.com>
2023-07-14 16:40:44 -0400

    Added comments and made slight changes to addButton onClickListner

--- app/src/main/java/com/majid/todolist/MainActivity.kt
+++ app/src/main/java/com/majid/todolist/MainActivity.kt
@@ -10,7 +10,11 @@ import com.majid.todolist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    // enabled binding in a module
+    // ActivityMainBinding creates a binding class for each XML layout file present in that module.
     private lateinit var binding: ActivityMainBinding
+
+    // Array to store the To-Dos user will input
     val userInputList = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +25,7 @@ class MainActivity : AppCompatActivity() {
         // for the UI of this activity
         setContentView(binding.root)
 
+        // Returns a view for userInputList and is used to display in ListView
         val arrayAdapter =
             ArrayAdapter<String>(
                 this,
@@ -30,6 +35,11 @@ class MainActivity : AppCompatActivity() {
 
         val userString = binding.userInput.getText()
 
+        // When add Button is clicked by user it will check if the user has entered a valid String
+        // If users doesn't enter anything and value is null
+        // A toast msg will be displayed to write a To-do
+        // If valid input a toast indicating added +String and
+        // setAdapter will set the layout
         binding.addButton.setOnClickListener {
             if (!(userString.toString() == "")) {
                 Toast.makeText(this, "Added " + userString, Toast.LENGTH_SHORT).show()
@@ -41,6 +51,10 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        // Delete a to-do string
+        // A toast message will be displayed indicating Deleted +String
+        // removeAt func will remove the item from list and
+        // setAdapter will set the layout
         binding.toDoList.onItemLongClickListener =
             AdapterView.OnItemLongClickListener { adapterView: AdapterView<*>, view2: View, i: Int, l: Long ->
                 Toast.makeText(
